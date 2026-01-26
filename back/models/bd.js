fbb44dbc0b53ad0f6b0703936503c66f8c4519e4
Rocío Vale <rociovale25@gmail.com>
2026-01-26 17:28:23 -0300

    Backend: agrego login y sesiones

---
+++ back/models/bd.js
@@ -0,0 +1,12 @@
+var mariadb = require('mariadb');
+var util = require('util');
+
+var pool = mariadb.createPool({
+    connectionLimit: 10,
+    host: process.env.MARIADB_HOST,
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_PASSWORD,
+    database: process.env.MARIADB_DATABASE
+});
+
+module.exports = pool;
