9a74f67042535668277672b0d60c4a6507c10d00
Rocío Vale <rociovale25@gmail.com>
2026-02-17 21:00:51 -0300

    envío form contacto

--- front/src/app/contacto/page.js
+++ front/src/app/contacto/page.js
@@ -6,35 +6,32 @@ export const metadata = {
     description: "descripcion de contacto"
 };
 
-export default function Contacto(){
-    return(
+export default function Contacto() {
+    return (
         <main className="holder cont">
 
-        <div className="titulo">
-            <h2>CONTACTO</h2>
-        </div>
-
-        <div className="contacto">
-            <div>
-                <h2>Contacto rápido</h2>
-                <ContactForm />
-    
+            <div className="titulo">
+                <h2>CONTACTO</h2>
             </div>
 
-            <div className="datos">
-                <h2>Otras vías de comunicación</h2>
-                <p>También puede contactarse con nosotros usando los siguientes medios.</p>
-                <ul>
-                    <li><i className="fa-solid fa-phone"></i> Telefono: 123456789</li>
-                    <li><i className="fa-solid fa-at"></i> Email: marusst@gmail.com</li>
-                    <li><i className="fa-brands fa-facebook"></i> Facebook: marusst</li>
-                    <li><i className="fa-brands fa-instagram"></i> Instagram: @marusst</li>
-                    <li><i className="fa-brands fa-whatsapp"></i> WhatsApp: 987654321</li>
+            <div className="contacto">
+                <div>
+                    <h2>Contacto rápido</h2>
+                    <ContactForm postUrl={`${process.env.API_BASE_URL}/api/contacto`} />
+                </div>
 
-                </ul>
+                <div className="datos">
+                    <h2>Otras vías de comunicación</h2>
+                    <p>También puede contactarse con nosotros usando los siguientes medios.</p>
+                    <ul>
+                        <li><i className="fa-solid fa-phone"></i> Telefono: 123456789</li>
+                        <li><i className="fa-solid fa-at"></i> Email: marusst@gmail.com</li>
+                        <li><i className="fa-brands fa-facebook"></i> Facebook: marusst</li>
+                        <li><i className="fa-brands fa-instagram"></i> Instagram: @marusst</li>
+                        <li><i className="fa-brands fa-whatsapp"></i> WhatsApp: 987654321</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-
-    </main>
+        </main >
     );
 }
--- front/src/components/contactForm.js
+++ front/src/components/contactForm.js
@@ -1,24 +1,77 @@
-export default function ContactForm() {
-    return (
-        <form action="/contacto" method="post" className="formulario">
-            <p>
-                <label>Nombre:</label>
-                <input type="text" name="nombre" />
-            </p>
-            <p>
-                <label>E-mail:</label>
-                <input type="text" name="email"/>
-            </p>
-            <p>
-                <label>Teléfono:</label>
-                <input type="text" name="telefono"/>
-            </p>
-            <p>
-                <label>Mensaje:</label>
-                <textarea name="comentario" id=""></textarea>
-            </p>
-            <p className="acciones"><input type="submit" name="" value="Enviar" /></p>
+'use client'
 
-        </form>
+import { useState } from 'react';
+
+export default function ContactForm({ postUrl }) {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        comentario: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true);
+
+        const rawResponse = await fetch(postUrl, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        });
+
+        const response = await rawResponse.json();
+        console.log(response);
+        setSending(false);
+        setMsg(response.message);
+        if (response.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
+    return (
+        <>
+            <form action="/contacto" method="post" onSubmit={handleSubmit} className="formulario">
+                <p>
+                    <label>Nombre:</label>
+                    <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} />
+                </p>
+                <p>
+                    <label>E-mail:</label>
+                    <input type="text" name="email" value={formData.email} onChange={handleChange} />
+                </p>
+                <p>
+                    <label>Teléfono:</label>
+                    <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
+                </p>
+                <p>
+                    <label>Mensaje:</label>
+                    <textarea name="comentario" id="" value={formData.comentario} onChange={handleChange}></textarea>
+                </p>
+                <p className="acciones"><input type="submit" name="" value="Enviar" /></p>
+
+            </form>
+
+            {sending ? <p>Enviando...</p> : null}
+
+            {msg ? <p>{msg}</p> : null}
+        </>
     );
 }
